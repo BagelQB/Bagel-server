201fdcbe3daf89f3f20de629cbcc104ca98308fb
Eamin Ahmed <bubblebyb@gmail.com>
2021-03-19 03:05:33 -0400

    added routes for getting bonuses and tossups by id

--- server.js
+++ server.js
@@ -2,4 +2,54 @@
 // Before your PR is accepted, you must write and pass your tests. (npm run test)
 // Server should be started with "npm run start".
 
+const express = require("express");
 
+const app = express();
+
+const questionService = require('./bagel_modules/questionService');
+
+app.listen(8080, () => {
+    console.log(`Server is running on port 8080.`);
+});
+
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get("/api/bonuses/", (req, res) => {
+    switch (req.query.type) {
+        case 'id':
+            questionService.getBonusByID(+req.query.id).then(({status, result}) => {
+                if (status === 'ok') {
+                    res.json({
+                        message: 'success',
+                        data: result
+                    });
+                } else if (status === 'fail') {
+                    res.json({
+                        message: 'error',
+                        error: result.error_code
+                    });
+                }
+            });
+    }
+});
+
+app.get("/api/tossups/", (req, res) => {
+    switch (req.query.type) {
+        case 'id':
+            questionService.getTossupByID(+req.query.id).then(({status, result}) => {
+                if (status === 'ok') {
+                    res.json({
+                        message: 'success',
+                        data: result
+                    });
+                } else if (status === 'fail') {
+                    res.json({
+                        message: 'error',
+                        error: result.error_code
+                    });
+                }
+            });
+    }
+});
